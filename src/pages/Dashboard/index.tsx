6caf16ad7a679f29f6688597a26e8ccf374a4468
allexis096 <allexissoares@gmail.com>
2020-08-01 14:22:31 -0300

    api connected

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useState, useCallback, FormEvent } from 'react';
 import { Container, Card, Content } from './styles';
+import { api } from '../../services/api';
+
+interface InputProps {
+  temp: number;
+}
 
 const Dashboard: React.FC = () => {
+  const [newCity, setNewCity] = useState('');
+  const [cities, setCities] = useState<InputProps[]>([]);
+
+  const handleSubmit = useCallback(
+    async (event: FormEvent<HTMLFormElement>): Promise<void> => {
+      event.preventDefault();
+
+      const response = await api.get(
+        `weather?format=json-cors&key=4bc8f9ad&city_name=${newCity}`,
+      );
+
+      const { results } = response.data;
+
+      setCities([...cities, results]);
+
+      console.log(results);
+    },
+    [newCity, cities],
+  );
+
   return (
     <Container>
       <Card>
-        <input type="text" placeholder="Get the temperature of your city" />
-        <button type="button">Find</button>
+        <form onSubmit={handleSubmit}>
+          <input
+            value={newCity}
+            onChange={e => setNewCity(e.target.value)}
+            type="text"
+            placeholder="Get the temperature of your city"
+          />
+          <button type="submit">Find</button>
+        </form>
       </Card>
-      <Content />
+      <Content>
+        {cities.map(city => (
+          <h1 key={city.temp}>{city.temp}</h1>
+        ))}
+      </Content>
     </Container>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -6,42 +6,48 @@ export const Container = styled.div`
 `;
 
 export const Card = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-  padding-top: 30px;
+    padding-top: 30px;
 
-  input {
-    width: 250px;
-    height: 50px;
+    input {
+      width: 250px;
+      height: 50px;
 
-    text-align: center;
+      text-align: center;
 
-    &::placeholder {
-      color: #aaa;
+      &::placeholder {
+        color: #aaa;
+      }
     }
-  }
 
-  button {
-    width: 250px;
-    height: 50px;
+    button {
+      width: 250px;
+      height: 50px;
 
-    margin-top: 5px;
+      margin-top: 5px;
 
-    border: none;
-    border-radius: 6px;
+      border: none;
+      border-radius: 6px;
 
-    color: #fff;
-    font-size: 24px;
+      color: #fff;
+      font-size: 24px;
 
-    background-color: #0b8a2d;
-    transition: background 0.2s;
+      background-color: #0b8a2d;
+      transition: background 0.2s;
 
-    &:hover {
-      background-color: ${darken(0.1, '#0b8a2d')};
+      &:hover {
+        background-color: ${darken(0.1, '#0b8a2d')};
+      }
     }
   }
 `;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+`;
